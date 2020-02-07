f6116354b7170f60591c4584f6c20fcf52807afb
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-07 19:50:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,11 +2,12 @@ def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |dwarf, index|
     puts "#{index + 1}.#{dwarf}"
-  end 
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map!{|array| array.capitalize + '!'}
 end
 
 def long_planeteer_calls# code an argument here
