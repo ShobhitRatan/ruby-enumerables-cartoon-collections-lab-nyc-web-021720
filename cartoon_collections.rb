2b01d44e82d04c34f013275da1a31e98a4bbefbb
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-07 19:53:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(array)# code an argument here
   array.map!{|array| array.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array_of_calls)# code an argument here
   # Your code here
+  array_of_calls.any? {|call| call.length > 4} 
 end
 
 def find_the_cheese# code an argument here
